7cd56c3ec759766493427a882238574117874558
Alexandr <alexandr_an.2006@mail.ru>
2024-01-31 20:42:31 +0600

    lab2

---
+++ Lab2/Boolean.py
@@ -0,0 +1,14 @@
+# Exercise 1
+print(10 > 9)  # Bool type:True
+
+# Exercise 2
+print(10 == 9)  # Bool type:False
+
+# Exercise 3
+print(10 < 9)  # Bool type:False
+
+# Exercise 4
+print(bool("abc"))  # Bool type:True
+
+# Exercise 5
+print(bool("0"))  # Bool type:False
